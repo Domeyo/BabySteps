6ed5e731acb4c53d1b9e8cd5455b400e9f6997ed
Alfred <leonallot@gmail.com>
2018-05-26 17:53:44 +0000

    validation fixed

--- Validator/EmailValidation.py
+++ Validator/EmailValidation.py
@@ -2,9 +2,15 @@
 import re
 
 def validateFormat(email):
-	pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-.]+$"
+	pattern = r"[\w+]{1,}@[\w+]{1,}\.[\w+]"
 	if re.match(pattern, email):
 		return True
 	return False
 
-
+if __name__=="__main__":
+	email = input('email = ')
+	if validateFormat(email):
+		print("valid")
+	else:
+		print("Invalid")
+	
